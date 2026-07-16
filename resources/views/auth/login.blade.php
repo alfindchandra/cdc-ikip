d6466c5ba46c6fab34eec92f905d86f0a99f8dea
alfindchandra <alfindwichandra55@gmail.com>
2026-07-16 07:17:27 +0700

    kerja3

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -304,7 +304,7 @@
             alerts.forEach(alert => {
                 alert.style.transition = 'opacity 0.5s';
                 alert.style.opacity = '0';
-                setTimeout(() => alert.remove(), 500);
+                setTimeout(() => alert.remove(), 500);  
             });
         }, 5000);
     </script>
